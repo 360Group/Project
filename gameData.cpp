ba5a851bf18f5b166c93d1650f208733f3755f1e
David Johnson <daj@daddydog.net>
2015-11-21 22:29:36 -0500

    added and modified functions of gamedata class

--- gameData.cpp
+++ gameData.cpp
@@ -2,18 +2,43 @@
 
 #include "gameData.h"
 
+//Constructer used to make a new game
 gameData(int id){
   for(int i = 0; i < 6; i++){
     for(int j = 0; j < 7; j++){
-      CFArray[i][j] = 'B';
+      board[i][j] = 'B';
     }
   }
 }
 
+//Return the array of data
 char[][] getArray(){
-  return CFArray;
+  return board;
 }
 
-int addPiece(int column){
- 
+/*Will try to add a piece to the column and will return an interger based on 
+ * success
+ * returns:
+ * 0- move successful
+ * 1- column is full and no more peices can be added
+ * */
+int addPiece(int column, char playerP){
+  for(int i = 5; i >= 0; i--){
+    if(board[i][column] == 'B'){
+      board[i][column] = playerP;
+      return 0;
+    }
+  }
+  return 1;
 } 
+
+/*return a string version of the array of the board*/
+string toString(){
+  string out;
+  for(int i = 0; i < 6; i++){
+    for(int j = 0; j < 7; j++){
+      out.push_back(board[i][j]);
+    }
+  }
+  return out;
+}
--- gameData.h
+++ gameData.h
@@ -1,14 +1,18 @@
 /*gameData.h*/
 
+#include <iostream>
+#include <string>
 class gameData{
   private:
     int gameID;
-    char CFArray[6][7];
-    gameData();
+    char board[6][7];
+    gameData(); //explicitly dissallow default constructor(cause I COMMAND IT!!!)
   public:
     gameData(int id): gameID(id);
     char[][] getArray();
-    char getIndex(int r, int c){return CFArray[r][c]};
-    int addPiece(int column);
+    char getIndex(int r, int c){return board[r][c];}
+    int addPiece(int column, char playerP);
+    int getGameID(){return gameID;}
+    string toString();
 }
 
